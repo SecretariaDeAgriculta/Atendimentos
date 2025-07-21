9fb1cffcfb7b238b18a7d5e55a54bc0c23065bda
conta teste <contateste6196924@gmail.com>
2025-07-21 10:59:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div>
    
    with the te

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import { Toaster } from "@/components/ui/toaster";
 export const metadata: Metadata = {
   title: 'Atendimento SEDER',
   description: 'Controle de Atendimentos',
+  icons: {
+    icon: '/favicon.ico',
+  }
 };
 
 export default function RootLayout({
